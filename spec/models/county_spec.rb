6cef59199c641e6b5ecfb5525288e68a47f107a5
Anvi Kunta <anvikunta@berkeley.edu>
2023-11-24 02:08:46 +0000

    fixed pronto check issues in user and county tests

--- spec/models/county_spec.rb
+++ spec/models/county_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe County do
   it 'returns standardized fibs code with correct number of digits' do
     county = described_class.new(fips_code: 1)
-    standardized_fips_score = county.std_fips_code
-    expect(standardized_fips_score).to eq('001')
+    expect(county.std_fips_code).to eq('001')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,33 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  before do
+    @google_user = described_class.create(first_name: 'Aadhi', last_name: 'Ghan', provider: 'google_oauth2', uid: '22')
+    @github_user = described_class.create(first_name: 'Anvi', last_name: 'Kunta', provider: 'github', uid: '2223')
+  end
+
   it 'name returns first and last name of user' do
-    user = described_class.new(first_name: 'Aadhiti', last_name: 'Ghankota')
-    name = user.name
-    expect(name).to eq('Aadhiti Ghankota')
+    expect(@google_user.name).to eq('Aadhi Ghan')
   end
 
   it 'returns Google as auth provider' do
-    user = described_class.new(first_name: 'Aadhiti', last_name: 'Ghankota', provider: 'google_oauth2')
-    auth = user.auth_provider
-    expect(auth).to eq('Google')
+    expect(@google_user.auth_provider).to eq('Google')
   end
 
   it 'returns Github as auth provider' do
-    user = described_class.new(first_name: 'Aadhiti', last_name: 'Ghankota', provider: 'github')
-    auth = user.auth_provider
-    expect(auth).to eq('Github')
+    expect(@github_user.auth_provider).to eq('Github')
   end
 
   it 'finds correct Google User' do
-    user = described_class.create(first_name: 'Aadhiti', last_name: 'Ghankota', provider: 'google_oauth2', uid: '2222')
-    found = described_class.find_google_user('2222')
-    expect(found.name).to eq(user.name)
+    found = described_class.find_google_user('22')
+    expect(found.name).to eq(@google_user.name)
   end
 
   it 'finds correct Github User' do
-    user = described_class.create(first_name: 'Aadhiti', last_name: 'Ghankota', provider: 'github', uid: '2222')
-    found = described_class.find_github_user('2222')
-    expect(found.name).to eq(user.name)
+    found = described_class.find_github_user('2223')
+    expect(found.name).to eq(@github_user.name)
   end
 end
