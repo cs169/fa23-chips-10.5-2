c36264412a29f2423a1e72d585355ad7d4bf53f8
Divya Mallemadugula <divya.malle@berkeley.edu>
2023-11-25 10:37:43 +0000

    fixed all failing tests

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -3,82 +3,6 @@
 class Representative < ApplicationRecord
   has_many :news_items, dependent: :delete_all
 
-  #   def self.find_official(options)
-  #     Representative.where(
-  #       'name = ? AND ocdid = ? AND title = ? AND ' \
-  #       'street = ? AND city = ? AND state = ? AND zip = ? AND ' \
-  #       'political_party = ? AND photo = ?',
-  #       options[:name],
-  #       options[:ocdid],
-  #       options[:title],
-  #       options[:street],
-  #       options[:city],
-  #       options[:state],
-  #       options[:zip],
-  #       options[:party],
-  #       options[:photo]
-  #     ).first
-  #   end
-
-  #   # def self.find_official(name, title, ocdid)
-  #   #   Representative.where('name = ? AND ocdid = ? AND title = ?', name, ocdid, title).first
-  #   # end
-
-  #   def self.civic_api_to_representative_params(rep_info)
-  #     reps = []
-
-  #     rep_info.officials.each_with_index do |official, index|
-  #       ocdid_temp = ''
-  #       title_temp = ''
-
-  #       rep_info.offices.each do |office|
-  #         if office.official_indices.include? index
-  #           title_temp = office.name
-  #           ocdid_temp = office.division_id
-  #         end
-  #       end
-
-  #       # to_add = find_official(official.name, title_temp, ocdid_temp)
-  #       # if to_add
-  #       #   reps.push(to_add)
-  #       # else
-  #       #   rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-  #       #       title: title_temp })
-  #       #   reps.push(rep)
-
-  #       to_add = find_official(
-  #         name:            official.name,
-  #         ocdid:           ocdid_temp,
-  #         title:           title_temp,
-  #         street:          official.address&.first&.line1,
-  #         city:            official.address&.first&.city,
-  #         state:           official.address&.first&.state,
-  #         zip:             official.address&.first&.zip,
-  #         political_party: official.party,
-  #         photo:           official.photo_url
-  #       )
-
-  #       if to_add
-  #         reps.push(to_add)
-  #       else
-  #         rep = Representative.create!(
-  #           name:            official.name,
-  #           ocdid:           ocdid_temp,
-  #           title:           title_temp,
-  #           street:          official.address&.first&.line1,
-  #           city:            official.address&.first&.city,
-  #           state:           official.address&.first&.state,
-  #           zip:             official.address&.first&.zip,
-  #           political_party: official.party,
-  #           photo:           official.photo_url
-  #         )
-  #         reps.push(rep)
-  #       end
-  #     end
-
-  #     reps
-  #   end
-
   def self.civic_api_to_representative_params(rep_info)
     reps = []
 
@@ -110,17 +34,6 @@ class Representative < ApplicationRecord
 
     [ocdid_temp, title_temp]
   end
-
-  # def self.find_or_create_representative(official, ocdid, title)
-  #   existing_rep = Representative.find_by(name: official.name, ocdid: ocdid, title: title,
-  #                                         street: official.address&.first&.line1, city: official.address&.first&.city,
-  #                                         state: official.address&.first&.state, zip: official.address&.first&.zip,
-  #                                         political_party: official.party, photo: official.photo_url)
-
-  #   return existing_rep if existing_rep
-
-  #   create_representative(official, ocdid, title)
-  # end
 
   def self.find_or_create_representative(official, ocdid, title)
     existing_rep = find_existing_representative(official, ocdid, title)
