e4fb2b09f0151a114feb64ae67890f4b5d87b544
Aadhiti Ghankota <aadhitighankota@berkeley.edu>
2023-11-25 08:31:29 +0000

    optimizing news_item test

--- spec/models/news_item_spec.rb
+++ spec/models/news_item_spec.rb
@@ -3,18 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe NewsItem do
-  rep = OpenStruct.new(id: 1234)
-  item = OpenStruct.new(representative: rep)
+  item = OpenStruct.new(representative: OpenStruct.new(id: 1234))
 
   it 'returns nil if a news item does not exist' do
     allow(described_class).to receive(:find_by).and_return(nil)
-    query = described_class.find_for(2)
+    query = described_class.find_for(0)
     expect(query).to be_nil
   end
 
   it 'finds a news item given a representative' do
     allow(described_class).to receive(:find_by).and_return(item)
-    query = described_class.find_for(rep.id)
+    query = described_class.find_for(1234)
     expect(query).to eq(item)
   end
 end
