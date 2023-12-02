9c25a3cec8664f4eb8a83975e6800e73359a02c8
Saif Ali <34604680+saiftali@users.noreply.github.com>
2023-12-01 18:53:02 -0800

    Update 20231122224129_change_political_party_data_type_to_string.rb

--- db/migrate/20231122224129_change_political_party_data_type_to_string.rb
+++ db/migrate/20231122224129_change_political_party_data_type_to_string.rb
@@ -1,7 +1,7 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-class ChangePoliticalPartyDataTypeToString < ActiveRecord::Migration[5.2]
-  def change
-    add_column :representatives, :political_party, :string
-  end
-end
+# class ChangePoliticalPartyDataTypeToString < ActiveRecord::Migration[5.2]
+#   def change
+#     add_column :representatives, :political_party, :string
+#   end
+# end
